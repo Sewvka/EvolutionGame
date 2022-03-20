6c72e4620f4d11d017a77b67d9568ae0e5ba9c1e
Sewvka <hoteroke@gmail.com>
2022-03-20 20:56:27 +0700

    Check if pass arg is Digit [WIN]

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 @ECHO OFF 
 TITLE Best team
 ECHO Please wait, I will run n nodes nodes
-@REM if %1% == --help (ECHO "args:\n n - count of noudes to create")
+SET "var="&for /f "delims=0123456789" %%i in ("%1") do set var=%%i
+if defined var (echo %1 NOT numeric; exit) else (echo %1 numeric)
 minikube start --nodes %1 -p node
