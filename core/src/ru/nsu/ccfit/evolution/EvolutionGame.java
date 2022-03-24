9634d2be6d8c945a4a0cfae924aefaec6c3c243c
Sewvka <hoteroke@gmail.com>
2022-03-24 11:48:53 +0700

    card movement

--- core/src/ru/nsu/ccfit/evolution/EvolutionGame.java
+++ core/src/ru/nsu/ccfit/evolution/EvolutionGame.java
@@ -1,9 +1,11 @@
 package ru.nsu.ccfit.evolution;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Rectangle;
@@ -15,19 +17,24 @@ public class EvolutionGame extends ApplicationAdapter {
 	Texture bigFatCard;
 	private Rectangle card;
 	Pixmap initPixmap, resizePixmap;
+	private OrthographicCamera camera;
+
 
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
 		card = new Rectangle();
-		initPixmap = new Pixmap(Gdx.files.internal("cards/big-fat.png"));
-		int size = 200;
+//		initPixmap = new Pixmap(Gdx.files.internal("cards/big-fat.png"));
+
+		int size = 75;
 		float coeff = 1.4f;
-		resizePixmap = new Pixmap(size, (int) (size*coeff), initPixmap.getFormat());
-		initPixmap.dispose();
-		bigFatCard = new Texture(resizePixmap);
-		resizePixmap.dispose();
+//		resizePixmap = new Pixmap(size, (int) (size*coeff), initPixmap.getFormat());
+//		initPixmap.dispose();
+		bigFatCard = new Texture(Gdx.files.internal("cards/big-fat.png"));
+//		resizePixmap.dispose();
 		card.x = 800 / 2 - size / 2;
 		card.y = 5;
 		card.height = (int) size*coeff;
@@ -38,9 +45,19 @@ public class EvolutionGame extends ApplicationAdapter {
 	@Override
 	public void render () {
 		ScreenUtils.clear(220, 220, 220, 0);
+		camera.update();
 		batch.begin();
 		batch.draw(bigFatCard, card.x, card.y);
 		batch.end();
+
+		if(Gdx.input.isTouched()) {
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			card.x = touchPos.x - card.getWidth() / 2;
+			card.y = touchPos.y - card.getHeight() / 2;
+		}
+
 	}
 	
 	@Override
