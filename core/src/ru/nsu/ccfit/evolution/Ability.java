22129e8e5f550b424affeb94a93b5ed2d5e2e959
Sewvka <hoteroke@gmail.com>
2022-04-05 09:03:25 +0700

    # Try to color chosen card

--- core/src/ru/nsu/ccfit/evolution/Ability.java
+++ core/src/ru/nsu/ccfit/evolution/Ability.java
@@ -1,5 +1,7 @@
 package ru.nsu.ccfit.evolution;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -10,9 +12,11 @@ import static com.badlogic.gdx.scenes.scene2d.actions.Actions.scaleTo;
 
 public class Ability extends GameActor {
     private Ability buddy;
+    private boolean chosen;
     public Ability(EvolutionGame game, float w, float h, int cardID, boolean firstAbility, Ability buddy) {
         super(w, h);
         this.buddy = buddy;
+        chosen = false;
         texture = new TextureRegion(game.getLoader().getTexture("cards/"+Cards.getName(cardID)+".png"));
         if (!firstAbility) texture.flip(true, true);
         addListener(new AbilityInputListener());
@@ -24,7 +28,21 @@ public class Ability extends GameActor {
 
     public void select() {
         addAction(scaleTo(1.1f, 1.1f, 0.2f));
+
         if (buddy != null) buddy.addAction(scaleTo(1.1f, 1.1f, 0.2f));
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        Color color = getColor();
+        if (chosen){
+        batch.enableBlending();
+        batch.setColor(0, 0, 255, color.a * parentAlpha);
+        } else {
+            batch.disableBlending();
+            batch.setColor(color.r, color.g, color.b, parentAlpha);
+        }
     }
 
     public void deselect() {
@@ -40,11 +58,13 @@ public class Ability extends GameActor {
     private class AbilityInputListener extends InputListener {
         @Override
         public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+            chosen = true;
             select();
         }
 
         @Override
         public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+            chosen = false;
             deselect();
         }
     }
