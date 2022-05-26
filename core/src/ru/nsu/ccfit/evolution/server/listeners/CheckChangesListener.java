7f85fcf4bffa157796ac0244e945631c20da3428
ymim <43496855+ymimsr@users.noreply.github.com>
2022-05-26 11:56:50 +0700

    wip

--- core/src/ru/nsu/ccfit/evolution/server/listeners/CheckChangesListener.java
+++ core/src/ru/nsu/ccfit/evolution/server/listeners/CheckChangesListener.java
@@ -1,7 +1,9 @@
 package ru.nsu.ccfit.evolution.server.listeners;
 
 import com.badlogic.gdx.utils.JsonValue;
+import ru.nsu.ccfit.evolution.server.CreatureModel;
 import ru.nsu.ccfit.evolution.server.GameWorldState;
+import ru.nsu.ccfit.evolution.server.TableModel;
 import ru.nsu.ccfit.evolution.user.framework.EvolutionGame;
 
 public class CheckChangesListener extends AbstractListener {
@@ -13,7 +15,18 @@ public class CheckChangesListener extends AbstractListener {
     @Override
     public void handle(JsonValue httpResponse) {
         if (httpResponse.has("game_stage")) {
-
+            httpResponse = httpResponse.get("game_stage");
+            if (httpResponse.has("creatures")) {
+                JsonValue creaturesJson = httpResponse.get("creatures");
+                for (JsonValue creatureJson = creaturesJson.child; creatureJson != null; creatureJson = creaturesJson.next) {
+                    int userID = creatureJson.get("player").getInt("id");
+                    int creatureID = creatureJson.getInt("id");
+                    TableModel userTable = gameWorldState.getTables().get(userID);
+                    if (userTable.getCreatures().containsKey(creatureID)) {
+                        CreatureModel creature = userTable.getCreatures().get(creatureID);
+                    }
+                }
+            }
         } else {
             logger.severe(
                     "Unexpected error in query to DB when checking a game, gameID: "
